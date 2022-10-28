b08d49546b0d5aa11cefe5f1e95f38d1ff5871b9
rayhanferdous <107350448+rayhanferdous@users.noreply.github.com>
2022-10-28 21:13:02 +0600

    footer fixed

--- components/Index/CallToAction.js
+++ components/Index/CallToAction.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function CallToAction() {
   return (
-    <div className="w-full h-72 bg-[url('/assets/content.png')] my-20">
-      <div className='py-16 pl-16'>
+    <div className="w-full h-72 bg-[url('/assets/content.png')] bg-contain bg-no-repeat mt-20">
+      <div className='py-8 pl-16'>
         <h1 className='font-Montserrat font-bold text-[40px] leading-[52px]'>Get started for free</h1>
         <p className='font-OpenSans font-normal text-xl leading-[40px]'>Turn your 3d ideas into incredible animations</p>
         <button className='font-Montserrat font-semibolo text-base leading-[14px] text-white bg-primary px-6 py-3 rounded-md mt-5'>Start 30 day trial</button>
--- components/Layout/Partials/Footer.js
+++ components/Layout/Partials/Footer.js
@@ -2,54 +2,63 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='grid grid-cols-5 bg-[#F8F9FA]'>
-      <div>
-        <h5  className=''>Product</h5>
+    <div className='bg-[#F8F9FA]'>
+    <div className='flex justify-between pt-20 pb-4'>
+      <div className=''>
+        <h5 className='font-Montserrat font-bold text-base leading-[24px]'>Product</h5>
         <ul>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-        </ul>
-      </div>
-      <div>
-        <h5>Platform</h5>
-        <ul>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-        </ul>
-      </div>
-      <div>
-        <h5>Resource</h5>
-        <ul>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-        </ul>
-      </div>
-      <div>
-        <h5>About</h5>
-        <ul>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Get started</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Updates</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Features</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Pricing</a></li>
         </ul>
       </div>
       <div className=''>
-        <h5>Support</h5>
+        <h5 className='font-Montserrat font-bold text-base leading-[24px]'>Platforms</h5>
         <ul>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
-          <li><a href="">Demo</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Android</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">iOS</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Web</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Unity</a></li>
         </ul>
       </div>
-      <div></div>
-      
+      <div className=''>
+        <h5 className='font-Montserrat font-bold text-base leading-[24px]'>Resources</h5>
+        <ul>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Tutorials</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Plagins</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Events</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Intregrations</a></li>
+        </ul>
+      </div>
+      <div className=''>
+        <h5 className='font-Montserrat font-bold text-base leading-[24px]'>About</h5>
+        <ul>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Community</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Company</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Jobs</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Blog</a></li>
+        </ul>
+      </div>
+      <div className=''>
+        <h5 className='font-Montserrat font-bold text-base leading-[24px]'>Support</h5>
+        <ul>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Help Center</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Tickets</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Contact</a></li>
+          <li><a className='font-OpenSans font-normal text-base leading-[32px]' href="">Forum</a></li>
+        </ul>
+      </div>      
+    </div>
+      <hr className='mt-5 mb-6 block' />
+    <div className='flex justify-between my-5'>
+        <p className='font-OpenSans font-normal text-[13px] leading-[24px]'>© 2020 Fabrx Design. All rights reserved.</p>
+        <div className='flex gap-x-3 h-4'>
+          <img src="/assets/social/fb.svg" alt="" />
+          <img src="/assets/social/dribble.svg" alt="" />
+          <img src="/assets/social/instra.svg" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
